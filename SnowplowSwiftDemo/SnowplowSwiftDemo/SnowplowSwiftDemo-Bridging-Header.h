1783774e931b811a226a21c1db030b3801d28fac
mhadam <michael@hadam.us>
2018-01-25 10:35:30 -0500

    Add a Swift port of SnowplowDemo (close #342)

---
+++ SnowplowSwiftDemo/SnowplowSwiftDemo/SnowplowSwiftDemo-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  SnowplowSwiftDemo-Bridging-Header.h
+//  SnowplowSwiftDemo
+//
+//  Created by Michael Hadam on 1/18/18.
+//  Copyright © 2018 snowplowanalytics. All rights reserved.
+//
+
+#ifndef SnowplowSwiftDemo_Bridging_Header_h
+#define SnowplowSwiftDemo_Bridging_Header_h
+
+#import "SnowplowTracker/SPEmitter.h"
+#import "SnowplowTracker/SPTracker.h"
+#import "SnowplowTracker/SPUtilities.h"
+#import "SnowplowTracker/SPSubject.h"
+#import "SnowplowTracker/SPRequestCallback.h"
+#import "SnowplowTracker/SPSelfDescribingJson.h"
+
+#endif /* SnowplowSwiftDemo_Bridging_Header_h */
